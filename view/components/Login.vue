402b919499d2b1af3e50c3c9b24c01c638c445dc
Tendi <tendifirmansyah30@gmail.com>
2023-05-19 22:40:57 +0700

    update basic ui

--- view/components/Login.vue
+++ view/components/Login.vue
@@ -1,42 +1,88 @@
 <template>
-  <div>
-    <input type="text" placeholder="username" v-model="username"/>
-    <input type="password" placeholder="password" v-model="password"/>
-    <button @click="doLogin">Login</button>
+  <div class="bg-white p-5 max-w-md mx-auto rounded shadow-sm">
+    <h2 class="text-4xl px-4">Log In</h2>
+    <div class="mt-10 space-y-8">
+      <input
+        v-model="username"
+        class="w-full border rounded h-12 px-4 focus:outline-none"
+        placeholder="Username "
+        type="text"
+      />
 
+      <div class="flex items-center">
+        <input
+          v-model="password"
+          class="w-full border rounded h-12 px-4 focus:outline-none -mr-7"
+          placeholder="Password"
+          type="password"
+        />
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="17.607"
+          height="17.076"
+          viewBox="0 0 17.607 17.076"
+        >
+          <path
+            id="eye-off"
+            d="M12.392,16.769a8.718,8.718,0,0,1-9.935-5.938A8.675,8.675,0,0,1,3.817,8.2m5.1.79a2.611,2.611,0,1,1,3.692,3.692M8.914,8.985,12.6,12.675M8.916,8.986,6.053,6.124m6.554,6.554,2.863,2.863M2.929,3,6.053,6.124m0,0a8.7,8.7,0,0,1,13.011,4.707,8.723,8.723,0,0,1-3.6,4.708m0,0,3.123,3.123"
+            transform="translate(-1.957 -2.293)"
+            fill="none"
+            stroke="#949090"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeidth="1"
+          />
+        </svg>
+      </div>
+      <div>
+        <div class="flex flex-col md:flex-row md:items-center justify-between">
+          <button
+            class="bg-orange-500 text-sm active:bg-gray-700 cursor-pointer font-regular text-white px-4 py-2 rounded uppercase"
+            @click="doLogin"
+          >
+            login now
+          </button>
+          <p
+            class="text-gray-400 text-sm underline self-center md:self-auto mt-4 md:mt-0"
+          >
+            Forgot your password?
+          </p>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
-import { userLoginRequest, userLoginResponse } from '../../proto/users_pb'
-
+import Vue from "vue";
+import { userLoginRequest, userLoginResponse } from "../../proto/users_pb";
 
 export default Vue.extend({
-  name: 'Login',
+  name: "Login",
   data: () => {
     return {
-      username : '',
-      password: ''
-    }
+      username: "",
+      password: "",
+    };
   },
-  mounted(){
-  },
+  mounted() {},
   methods: {
-    async doLogin(){
-      if(!this.username || !this.password){
-        alert('fill username & password')
-        return
+    async doLogin() {
+      if (!this.username || !this.password) {
+        alert("fill username & password");
+        return;
       }
-      let request = new userLoginRequest()
-      request.setUsername(this.username)
-      request.setPassword(this.password)
-      let response = await <Promise<userLoginResponse>>this.$store.dispatch('login', request)
-      if(response){
-        this.$store.commit('setToken', response.getToken())
-        this.$router.push('/customers')
+      let request = new userLoginRequest();
+      request.setUsername(this.username);
+      request.setPassword(this.password);
+      let response = await (<Promise<userLoginResponse>>(
+        this.$store.dispatch("login", request)
+      ));
+      if (response) {
+        this.$store.commit("setToken", response.getToken());
+        this.$router.push("/customers");
       }
-    }
-  }
-})
+    },
+  },
+});
 </script>
--- view/pages/customers.vue
+++ view/pages/customers.vue
@@ -4,26 +4,167 @@
       <input type="file" @input="upload_excel($event)" />
     </form>
     <button @click="export_excel">Export</button>
-    <table>
-      <thead>
-        <tr>
-          <th>Nama</th>
-          <th>KK</th>
-          <th>Desa/Kampung</th>
-          <th>PJ</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="customer in dataList" v-bind:key="customer._id">
-          <td>{{ customer.nama }}</td>
-          <td>{{ customer.nokk }}</td>
-          <td>
-            {{ customer.kampung ? customer.kampung : customer.desakelurahan }}
-          </td>
-          <td>{{ customer.pj }}</td>
-        </tr>
-      </tbody>
-    </table>
+    <div class="flex flex-col">
+      <div class="overflow-x-auto sm:-mx-6 lg:-mx-8">
+        <div class="inline-block min-w-full py-2 sm:px-6 lg:px-8">
+          <div class="overflow-hidden">
+            <table class="min-w-full text-left text-sm font-light">
+              <thead class="border-b font-medium dark:border-neutral-500">
+                <tr>
+                  <th scope="col" class="px-6 py-4">Nama</th>
+                  <th scope="col" class="px-6 py-4">KK</th>
+                  <th scope="col" class="px-6 py-4">Desa/Kampung</th>
+                  <th scope="col" class="px-6 py-4">PJ</th>
+                  <th scope="col" class="px-6 py-4">Status</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr
+                  class="border-b dark:border-neutral-500"
+                  v-for="customer in dataList"
+                  v-bind:key="customer._id"
+                >
+                  <td class="whitespace-nowrap px-6 py-4 font-medium">
+                    {{ customer.nama }}
+                  </td>
+                  <td class="whitespace-nowrap px-6 py-4">
+                    {{ customer.nokk }}
+                  </td>
+                  <td class="whitespace-nowrap px-6 py-4">
+                    {{
+                      customer.kampung
+                        ? customer.kampung
+                        : customer.desakelurahan
+                    }}
+                  </td>
+                  <td class="whitespace-nowrap px-6 py-4">{{ customer.pj }}</td>
+                  <td class="whitespace-nowrap px-6 py-4">
+                    <button
+                      class="bg-blue-500 text-sm active:bg-gray-700 cursor-pointer font-regular text-white px-4 py-2 rounded uppercase"
+                    >
+                      TUNAI
+                    </button>
+                    <button
+                      class="bg-green-500 text-sm active:bg-gray-700 cursor-pointer font-regular text-white px-4 py-2 rounded uppercase"
+                    >
+                      TERBINA
+                    </button>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div
+      class="flex items-center justify-between border-t border-gray-200 bg-white px-4 py-3 sm:px-6"
+    >
+      <div class="flex flex-1 justify-between sm:hidden">
+        <a
+          href="#"
+          class="relative inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50"
+          >Previous</a
+        >
+        <a
+          href="#"
+          class="relative ml-3 inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50"
+          >Next</a
+        >
+      </div>
+      <div class="hidden sm:flex sm:flex-1 sm:items-center sm:justify-between">
+        <div>
+          <p class="text-sm text-gray-700">
+            Showing
+            <span class="font-medium">1</span>
+            to
+            <span class="font-medium">10</span>
+            of
+            <span class="font-medium">97</span>
+            results
+          </p>
+        </div>
+        <div>
+          <nav
+            class="isolate inline-flex -space-x-px rounded-md shadow-sm"
+            aria-label="Pagination"
+          >
+            <a
+              href="#"
+              class="relative inline-flex items-center rounded-l-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
+            >
+              <span class="sr-only">Previous</span>
+              <svg
+                class="h-5 w-5"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+                aria-hidden="true"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M12.79 5.23a.75.75 0 01-.02 1.06L8.832 10l3.938 3.71a.75.75 0 11-1.04 1.08l-4.5-4.25a.75.75 0 010-1.08l4.5-4.25a.75.75 0 011.06.02z"
+                  clip-rule="evenodd"
+                />
+              </svg>
+            </a>
+            <!-- Current: "z-10 bg-indigo-600 text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600", Default: "text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:outline-offset-0" -->
+            <a
+              href="#"
+              aria-current="page"
+              class="relative z-10 inline-flex items-center bg-indigo-600 px-4 py-2 text-sm font-semibold text-white focus:z-20 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >1</a
+            >
+            <a
+              href="#"
+              class="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
+              >2</a
+            >
+            <a
+              href="#"
+              class="relative hidden items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0 md:inline-flex"
+              >3</a
+            >
+            <span
+              class="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-700 ring-1 ring-inset ring-gray-300 focus:outline-offset-0"
+              >...</span
+            >
+            <a
+              href="#"
+              class="relative hidden items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0 md:inline-flex"
+              >8</a
+            >
+            <a
+              href="#"
+              class="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
+              >9</a
+            >
+            <a
+              href="#"
+              class="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
+              >10</a
+            >
+            <a
+              href="#"
+              class="relative inline-flex items-center rounded-r-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
+            >
+              <span class="sr-only">Next</span>
+              <svg
+                class="h-5 w-5"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+                aria-hidden="true"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M7.21 14.77a.75.75 0 01.02-1.06L11.168 10 7.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z"
+                  clip-rule="evenodd"
+                />
+              </svg>
+            </a>
+          </nav>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -72,8 +213,6 @@ export default Vue.extend({
         obj.dataList.map((customer: Customer.AsObject) => {
           this.dataList.push(customer);
         });
-
-        console.log("response", obj);
       }
     },
     async upload_excel(event: any) {
@@ -110,7 +249,6 @@ export default Vue.extend({
           customer.setKol(customerData["Kol"]);
           customer.setSyahidan(customerData["Syahidan"]);
           customer.setPj(customerData["PJ"]);
-          console.log('customer', customer.toObject())
           customers.push(customer);
         });
         customerCreateRequest.setDataList(customers);
@@ -148,12 +286,11 @@ export default Vue.extend({
           "RW",
           "Kol",
           "Syahidan",
-          "PJ"
+          "PJ",
         ],
       ];
 
       for (const customer of customers) {
-        console.log('customer', customer.toObject())
         data.push([
           customer.getId(),
           customer.getNik(),
@@ -170,8 +307,8 @@ export default Vue.extend({
           customer.getRw(),
           customer.getKol(),
           customer.getSyahidan(),
-          customer.getPj()
-        ],)
+          customer.getPj(),
+        ]);
       }
       var filename = "customers.xlsx";
 
