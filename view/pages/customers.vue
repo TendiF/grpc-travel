8a5f183a5bba8ed28a9531be9a6660eb8503dc0e
Tendi <tendifirmansyah30@gmail.com>
2023-05-20 23:14:53 +0700

    fix search bug

--- view/pages/customers.vue
+++ view/pages/customers.vue
@@ -10,7 +10,7 @@
         <div class="relative mt-2 rounded-md shadow-sm">
           <input
             v-model="search"
-            @input="getCustomers"
+            @input="page = 1; getCustomers()"
             type="text"
             name="price"
             class="block w-full rounded-md border-0 py-1.5 pl-2 pr-5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
